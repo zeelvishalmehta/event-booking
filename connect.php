39515b1a3ddf32210a68efdd42dc452925a5dadc
Zeel Mehta <34303039+zeelvishalmehta@users.noreply.github.com>
2021-08-25 17:30:48 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,8 @@
 <?php 
-//$link = mysqli_connect("localhost", "event-booking", "hA0~ua58");
-//mysqli_select_db($link,"event-booking");
-
 $servername = "localhost";
-$username = "event-booking";
-$password = "hA0~ua58";
-$dbname = "event-booking";
+$username = "***";
+$password = "***";
+$dbname = "***";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
